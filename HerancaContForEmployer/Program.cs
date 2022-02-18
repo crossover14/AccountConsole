33137b82fbcee5ef656218db7b564e43c79cfc1c
LeandroVPordeus <58612986+LeandroVPordeus@users.noreply.github.com>
2022-02-17 22:09:27 -0300

    Inicio

---
+++ HerancaContForEmployer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HerancaContForEmployer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
